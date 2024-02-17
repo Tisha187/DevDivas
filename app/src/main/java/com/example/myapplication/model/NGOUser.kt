76867081da834c3a607a019f6f24587612d4a0a6
Tushant <xyz@gmail.com>
2024-02-17 19:29:03 +0530

    NGO Authenatication Done

---
+++ app/src/main/java/com/example/myapplication/model/NGOUser.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.model
+
+data class NGOUser(
+    val organizationName: String? = null,
+    val email: String? = null,
+    val password: String? = null,
+    val publishedYear: String? = null,
+    val city: String? = null
+)
