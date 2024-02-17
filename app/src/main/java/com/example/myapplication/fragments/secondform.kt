10fb80c9243c73fb3a3fd78bc7e8bfcb3dbf37b1
kushwahanimisha <146454562+kushwahanimisha@users.noreply.github.com>
2024-02-17 16:45:24 +0530

    recycler view added

--- app/src/main/java/com/example/myapplication/fragments/secondform.kt
+++ app/src/main/java/com/example/myapplication/fragments/secondform.kt
@@ -1,13 +1,18 @@
 package com.example.myapplication.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.Adopt_Activity
+import com.example.myapplication.ChildProfile
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentDashboardfragmentBinding
 import com.example.myapplication.databinding.FragmentFormBinding
+import com.example.myapplication.databinding.FragmentSecondformBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,14 +25,21 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class secondform : Fragment() {
-    private var binding: FragmentFormBinding? = null
+    private var binding: FragmentSecondformBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_secondform, container, false)
+        binding = FragmentSecondformBinding.inflate(inflater, container, false)
+        return binding?.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
